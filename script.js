50390c5bed981f9ca3e222ede3dfdc5582d9c682
chiyotasuke <36558814+chiyotasuke@users.noreply.github.com>
2023-01-11 20:42:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /* To make this, enable "One Column" option in SumoDB, copy & paste the tables 
  * as plain text and then turn them into array like this. Don't forget to add 
  * the empty spots in the banzuke (as empty string ""). Put the character ' ' 
@@ -384,9 +383,9 @@ window.onload = function() {
         updateSave();
 
       document.getElementById("progressText").innerHTML = "Saved!";
-      loadSaveButton.disabled = false;
       setTimeout(function() {
         showSave();
+        loadSaveButton.disabled = false;
         document.getElementById("progressText").innerHTML = "";
       }, 2000);
     }
