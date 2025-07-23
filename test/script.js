3359e5c389e442e58856378239552f1c2b814be4
John Clyde <johnclyde@gmail.com>
2025-07-22 17:50:42 -0700

    Move rikishi data arrays to rikishi-names.js where they belong

--- test/script.js
+++ test/script.js
@@ -1,176 +1,9 @@
-
-/* To make this, enable "One Column" option in SumoDB, copy & paste the tables 
- * as plain text and then turn them into array like this. Don't forget to add 
- * the empty spots in the banzuke (as empty string ""). Put the character ' ' 
- * in between the record and special letter Y, S, DK ... As ' ' 
- * is not considered a regular whitespace, it will not expand.
- */ 
-var theSekitori = [
-  "Y1e Hoshoryu 0-0", 
-  "Y1w Onosato 0-0", 
-  "O1e Kotozakura 0-0", 
-  "", 
-  "S1e Wakatakakage 0-0", 
-  "", 
-  "S2e Takayasu 0-0", 
-  "S2w Shodai 0-0", 
-  "K1e Kirishima 0-0", 
-  "", 
-  "K2e Meisei 0-0", 
-  "K2w Wakamotoharu 0-0", 
-  "M1e Tobizaru 0-0", 
-  "M1w Daieisho 0-0", 
-  "M2e Mitakeumi 0-0", 
-  "M2w Tamawashi 0-0", 
-  "M3e Abi 0-0", 
-  "M3w Midorifuji 0-0", 
-  "M4e Nishikifuji 0-0", 
-  "M4w Sadanoumi 0-0", 
-  "M5e Ryuden 0-0", 
-  "M5w Nishikigi 0-0", 
-  "M6e Hokutofuji 0-0", 
-  "M6w Myogiryu 0-0", 
-  "M7e Ichinojo 0-0", 
-  "M7w Ura 0-0", 
-  "M8e Onosho 0-0", 
-  "M8w Oho 0-0", 
-  "M9e Takanosho 0-0", 
-  "M9w Endo 0-0", 
-  "M10e Aoiyama 0-0", 
-  "M10w Hiradoumi 0-0", 
-  "M11e Chiyoshoma 0-0", 
-  "M11w Tochinoshin 0-0", 
-  "M12e Kagayaki 0-0", 
-  "M12w Okinoumi 0-0", 
-  "M13e Kotoshoho 0-0", 
-  "M13w Kotoeko 0-0", 
-  "M14e Ichiyamamoto 0-0", 
-  "M14w Azumaryu 0-0", 
-  "M15e Tsurugisho 0-0", 
-  "M15w Mitoryu 0-0", 
-  "M16e Takarafuji 0-0", 
-  "M16w Chiyomaru 0-0", 
-  "M17e Kusano 0-0",
-  "M17w Fujinokawa 0-0",
-  "J1e Akua 0-0", 
-  "J1w Bushozan 0-0", 
-  "J2e Hokuseiho 0-0", 
-  "J2w Daiamami 0-0", 
-  "J3e Atamifuji 0-0", 
-  "J3w Oshoma 0-0", 
-  "J4e Tohakuryu 0-0", 
-  "J4w Enho 0-0", 
-  "J5e Kinbozan 0-0", 
-  "J5w Kotokuzan 0-0", 
-  "J6e Churanoumi 0-0", 
-  "J6w Daishoho 0-0", 
-  "J7e Chiyonokuni 0-0", 
-  "J7w Tochimusashi 0-0", 
-  "J8e Shimanoumi 0-0", 
-  "J8w Kitanowaka 0-0", 
-  "J9e Roga 0-0", 
-  "J9w Hidenoumi 0-0", 
-  "J10e Gonoyama 0-0", 
-  "J10w Terutsuyoshi 0-0", 
-  "J11e Chiyosakae 0-0", 
-  "J11w Shimazuumi 0-0", 
-  "J12e Takakento 0-0", 
-  "J12w Asanoyama 0-0", 
-  "J13e Shonannoumi 0-0", 
-  "J13w Kaisho 0-0", 
-  "J14e Tsushimanada 0-0", 
-  "J14w Hakuyozan 0-0"
-];
-
-/* Enable "No Rank Colouring" and "One Column" options and then open the 
- * browser's inspector (F12). Find the table and copy & paste the <tbody> node. 
- * The rikishi ID is located right after "Rikishi.aspx?r=". Turn the IDs into an 
- * array (add the empty spots as 0). This array should have the same length as 
- * theSekitori array.
- */
-var sekitoriID = [
-  12451, 
-  12453, 
-  12270, 
-  0, 
-  12370, 
-  0, 
-  6480, 
-  12130, 
-  12231, 
-  0, 
-  11946, 
-  11980, 
-  12203, 
-  11985, 
-  12210, 
-  5944, 
-  12094, 
-  12352, 
-  12351, 
-  2879, 
-  6594, 
-  6596, 
-  12239, 
-  11784, 
-  12107, 
-  12226, 
-  12043, 
-  12453, 
-  11855, 
-  12055, 
-  11786, 
-  12314, 
-  11785, 
-  6599, 
-  11845, 
-  6463, 
-  12449, 
-  7153, 
-  12362, 
-  11723, 
-  12113, 
-  12406, 
-  11728, 
-  7240, 
-  12000,
-  12001,
-  11918, 
-  12117, 
-  12646, 
-  12273, 
-  12664, 
-  12717, 
-  12575, 
-  12412, 
-  12721, 
-  11809, 
-  12320, 
-  12040, 
-  6642, 
-  12674, 
-  12024, 
-  12548, 
-  12516, 
-  12026, 
-  12688, 
-  11868, 
-  11736, 
-  12013, 
-  12114, 
-  12291, 
-  12162, 
-  12075, 
-  12342, 
-  11943
-];
-
 //***** Just update the "basho" variable and you're all done. *****
 
+// These need to be global for other functions to access
+var basho = "202301"; // The date of the basho just ended
+
 window.onload = function() {
-
-
-  var basho = "202301"; // The date of the basho just ended
 
   if (window.localStorage.getItem("banzuke1") !== null) {
     window.localStorage.removeItem("banzuke1");
@@ -437,16 +270,16 @@ redips.init = function () {
             else if (thisChg == 0) 
               thisChg = "─";
             break;
-          case 'J': thisChg = " ↑ "; break;
+          case 'J': thisChg = " ↑ "; break;
           default:
-            thisChg = " ↓ ";
+            thisChg = " ↓ ";
             thisRank = thisRank.substring(0, 1);
         }
       }
       else if (targetCellRank.charAt(0) == 'J') {
         targetCellRank = 'J';
         switch (thisRank.charAt(0)) {
-          case 'M': thisChg = " ↓ "; break;
+          case 'M': thisChg = " ↓ "; break;
           case 'J': thisChg = "⇄"; break;
           default:  thisChg = "!!!";
         }
@@ -454,7 +287,7 @@ redips.init = function () {
       else {
         switch (thisRank.charAt(0)) {
           case 'M': 
-            thisChg = " ↑ ";
+            thisChg = " ↑ ";
             targetCellRank = targetCellRank.substring(0, 1);
             break;
           case 'J': thisChg = "!!!"; break;
