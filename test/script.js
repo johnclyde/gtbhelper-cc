2dad426d50b4ad2df12318c60bb1e282cb9bb7b7
John Clyde <175860+johnclyde@users.noreply.github.com>
2025-07-22 17:55:09 -0700

    Remove Google Drive integration and move rikishi data to proper module
    - Removed all Google Drive functionality from the application (~240 lines of code)
    - Moved rikishi data arrays (theSekitori and sekitoriID) from script.js to rikishi-names.js where they logically belong
    - Removed Google API script imports from HTML files
    - Removed save/load UI elements from the interface
    - Fixed initialization issues to ensure the application continues working with localStorage only

--- test/script.js
+++ test/script.js
@@ -1,417 +1,9 @@
-
-/* To make this, enable "One Column" option in SumoDB, copy & paste the tables 
- * as plain text and then turn them into array like this. Don't forget to add 
- * the empty spots in the banzuke (as empty string ""). Put the character ' ' 
- * in between the record and special letter Y, S, DK ... As ' ' 
- * is not considered a regular whitespace, it will not expand.
- */ 
-var theSekitori = [
-  "Y1e Hoshoryu 0-0", 
-  "Y1w Onosato 0-0", 
-  "O1e Kotozakura 0-0", 
-  "", 
-  "S1e Wakatakakage 0-0", 
-  "", 
-  "S2e Takayasu 0-0", 
-  "S2w Shodai 0-0", 
-  "K1e Kirishima 0-0", 
-  "", 
-  "K2e Meisei 0-0", 
-  "K2w Wakamotoharu 0-0", 
-  "M1e Tobizaru 0-0", 
-  "M1w Daieisho 0-0", 
-  "M2e Mitakeumi 0-0", 
-  "M2w Tamawashi 0-0", 
-  "M3e Abi 0-0", 
-  "M3w Midorifuji 0-0", 
-  "M4e Nishikifuji 0-0", 
-  "M4w Sadanoumi 0-0", 
-  "M5e Ryuden 0-0", 
-  "M5w Nishikigi 0-0", 
-  "M6e Hokutofuji 0-0", 
-  "M6w Myogiryu 0-0", 
-  "M7e Ichinojo 0-0", 
-  "M7w Ura 0-0", 
-  "M8e Onosho 0-0", 
-  "M8w Oho 0-0", 
-  "M9e Takanosho 0-0", 
-  "M9w Endo 0-0", 
-  "M10e Aoiyama 0-0", 
-  "M10w Hiradoumi 0-0", 
-  "M11e Chiyoshoma 0-0", 
-  "M11w Tochinoshin 0-0", 
-  "M12e Kagayaki 0-0", 
-  "M12w Okinoumi 0-0", 
-  "M13e Kotoshoho 0-0", 
-  "M13w Kotoeko 0-0", 
-  "M14e Ichiyamamoto 0-0", 
-  "M14w Azumaryu 0-0", 
-  "M15e Tsurugisho 0-0", 
-  "M15w Mitoryu 0-0", 
-  "M16e Takarafuji 0-0", 
-  "M16w Chiyomaru 0-0", 
-  "M17e Kusano 0-0",
-  "M17w Fujinokawa 0-0",
-  "J1e Akua 0-0", 
-  "J1w Bushozan 0-0", 
-  "J2e Hokuseiho 0-0", 
-  "J2w Daiamami 0-0", 
-  "J3e Atamifuji 0-0", 
-  "J3w Oshoma 0-0", 
-  "J4e Tohakuryu 0-0", 
-  "J4w Enho 0-0", 
-  "J5e Kinbozan 0-0", 
-  "J5w Kotokuzan 0-0", 
-  "J6e Churanoumi 0-0", 
-  "J6w Daishoho 0-0", 
-  "J7e Chiyonokuni 0-0", 
-  "J7w Tochimusashi 0-0", 
-  "J8e Shimanoumi 0-0", 
-  "J8w Kitanowaka 0-0", 
-  "J9e Roga 0-0", 
-  "J9w Hidenoumi 0-0", 
-  "J10e Gonoyama 0-0", 
-  "J10w Terutsuyoshi 0-0", 
-  "J11e Chiyosakae 0-0", 
-  "J11w Shimazuumi 0-0", 
-  "J12e Takakento 0-0", 
-  "J12w Asanoyama 0-0", 
-  "J13e Shonannoumi 0-0", 
-  "J13w Kaisho 0-0", 
-  "J14e Tsushimanada 0-0", 
-  "J14w Hakuyozan 0-0"
-];
-
-/* Enable "No Rank Colouring" and "One Column" options and then open the 
- * browser's inspector (F12). Find the table and copy & paste the <tbody> node. 
- * The rikishi ID is located right after "Rikishi.aspx?r=". Turn the IDs into an 
- * array (add the empty spots as 0). This array should have the same length as 
- * theSekitori array.
- */
-var sekitoriID = [
-  12451, 
-  12453, 
-  12270, 
-  0, 
-  12370, 
-  0, 
-  6480, 
-  12130, 
-  12231, 
-  0, 
-  11946, 
-  11980, 
-  12203, 
-  11985, 
-  12210, 
-  5944, 
-  12094, 
-  12352, 
-  12351, 
-  2879, 
-  6594, 
-  6596, 
-  12239, 
-  11784, 
-  12107, 
-  12226, 
-  12043, 
-  12453, 
-  11855, 
-  12055, 
-  11786, 
-  12314, 
-  11785, 
-  6599, 
-  11845, 
-  6463, 
-  12449, 
-  7153, 
-  12362, 
-  11723, 
-  12113, 
-  12406, 
-  11728, 
-  7240, 
-  12000,
-  12001,
-  11918, 
-  12117, 
-  12646, 
-  12273, 
-  12664, 
-  12717, 
-  12575, 
-  12412, 
-  12721, 
-  11809, 
-  12320, 
-  12040, 
-  6642, 
-  12674, 
-  12024, 
-  12548, 
-  12516, 
-  12026, 
-  12688, 
-  11868, 
-  11736, 
-  12013, 
-  12114, 
-  12291, 
-  12162, 
-  12075, 
-  12342, 
-  11943
-];
-
 //***** Just update the "basho" variable and you're all done. *****
 
+// These need to be global for other functions to access
+var basho = "202301"; // The date of the basho just ended
+
 window.onload = function() {
-
-  var CLIENT_ID = "527214845927-p6ofscooll9ettfc8vpb4f5dqbhome4h.apps.googleusercontent.com";
-  var API_KEY = "AIzaSyBiIfRASPUPjYmDLggGBQKCw63h-5B073o";
-  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
-  // https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid
-  var SCOPES = "https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.appfolder https://www.googleapis.com/auth/drive.install https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.resource";
-  var signinButton = document.getElementById("signinButton");
-  var signoutButton = document.getElementById("signoutButton");
-  var saveToDriveButton = document.getElementById("saveToDrive");
-  var loadSaveButton = document.getElementById("loadSave");
-  var messageLine = document.getElementById("messageLine");
-  let tokenClient;
-  let gapiInited = false;
-  let gisInited = false;
-
-  signinButton.style.display = "none";
-  signoutButton.style.display = "none";
-  saveToDriveButton.style.display = "none";
-  loadSaveButton.style.display = "none";
-
-  gapiLoaded();
-  gisLoaded();
-
-  function gapiLoaded() {
-    gapi.load("client", initializeGapiClient);
-  }
-
-  async function initializeGapiClient() {
-    await gapi.client.init({
-      apiKey: API_KEY,
-      discoveryDocs: DISCOVERY_DOCS
-    });
-    gapiInited = true;
-    maybeEnableButtons();
-  }
-
-  function gisLoaded() {
-    tokenClient = google.accounts.oauth2.initTokenClient({
-      client_id: CLIENT_ID,
-      scope: SCOPES, 
-      prompt: "", 
-      callback: ""
-    });
-    gisInited = true;
-    maybeEnableButtons();
-  }
-
-  function maybeEnableButtons() {
-    if (gapiInited && gisInited) 
-      signinButton.style.display = "inline-block";
-  }
-
-  signinButton.onclick = () => handleAuthClick()
-  function handleAuthClick() {
-    tokenClient.callback = async (resp) => {
-      if (resp.error !== undefined) 
-        throw (resp);
-      signinButton.style.display = "none";
-      signoutButton.style.display = "inline-block";
-      saveToDriveButton.style.display = "inline-block";
-      loadSaveButton.style.display = "inline-block";
-      messageLine.innerHTML = "Please wait...";
-      checkFolder("GTB Helper Save (do not modify)");
-    };
-
-    if (gapi.client.getToken() === null) 
-      tokenClient.requestAccessToken({ prompt: "consent" });
-    else 
-      tokenClient.requestAccessToken({ prompt: "" });
-  }
-
-  signoutButton.onclick = () => handleSignoutClick()
-  function handleSignoutClick() {
-    const token = gapi.client.getToken();
-
-    if (token !== null) {
-      google.accounts.oauth2.revoke(token.access_token);
-      gapi.client.setToken("");
-      signinButton.style.display = "inline-block";
-      signoutButton.style.display = "none";
-      saveToDriveButton.style.display = "none";
-      loadSaveButton.style.display = "none";
-      messageLine.innerHTML = "Save or load your banzuke";
-      document.getElementById("progressText").innerHTML = "";
-    }
-  }
-
-  function checkFolder(folderName) {
-    gapi.client.drive.files.list({
-      'q': "name = '" + folderName + "'"
-    }).then(function (response) {
-      var files = response.result.files;
-
-      if (files && files.length > 0) {
-        for (var i = 0; i < files.length; i++) {
-          var file = files[i];
-
-          window.localStorage.setItem("backupFolderId", file.id);
-          //console.log("Folder available");
-          showSave();
-        }
-      }
-      else {
-        createFolder("GTB Helper Save (do not modify)");
-        messageLine.innerHTML = "No save";
-      }
-    })
-  }
-
-  function createFolder(folderName) {
-    var access_token = gapi.auth.getToken().access_token;
-    var request = gapi.client.request({
-      "path": "drive/v2/files",
-      "method": "POST",
-      "headers": {
-        "Content-Type": "application/json", 
-        "Authorization": "Bearer " + access_token
-      }, 
-      "body": {
-        "title": folderName, 
-        "mimeType": "application/vnd.google-apps.folder"
-      }
-    });
-    request.execute(function (response) {
-      window.localStorage.setItem("backupFolderId", response.id);
-    })
-  }
-
-  function showSave() {
-    var saveId = "";
-
-    gapi.client.drive.files.list({
-      'q': "name = 'gtb_helper_save.txt' and parents in '" + 
-           window.localStorage.getItem("backupFolderId") + "'"
-    }).then(function (response) {
-      var files = response.result.files;
-
-      if (files && files.length > 0) {
-        for (var i = 0; i < files.length; i++) {
-          var saveId = files[i].id;
-
-          gapi.client.drive.files.get({
-            "fileId": saveId, 
-            "fields": "modifiedTime"
-          }).then(function (res) {
-            var modifiedTime = moment(res.result.modifiedTime, 
-              "YYYY-MM-DDThh:mm:ss.SSSZ").format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-            messageLine.setAttribute("data-saveId", saveId);
-            messageLine.innerHTML = "From " + modifiedTime;
-            loadSaveButton.disabled = false;
-          })
-        }
-      }
-      else {
-        loadSaveButton.disabled = true;
-        messageLine.innerHTML = "No save";
-      }
-    })
-  }
-
-  function uploadSave() {
-    const blob = new Blob([window.localStorage.getItem("banzuke")], { type: "plain/text" });
-    const parentFolder = window.localStorage.getItem("backupFolderId");
-    var metadata = {
-      name: "gtb_helper_save.txt", 
-      mimeType: "plain/text", 
-      parents: [parentFolder]
-    };
-    var formData = new FormData();
-
-    formData.append("metadata", new Blob([JSON.stringify(metadata)], { type: "application/json" }));
-    formData.append("file", blob);
-
-    fetch("https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart", {
-      method: "POST", 
-      headers: new Headers({ "Authorization": "Bearer " + gapi.auth.getToken().access_token }), 
-      body: formData
-    }).then(function (response) {
-      return response.json();
-    }).then(function (value) {
-      //console.log(value);
-      //showSave();
-    });
-  }
-
-  function updateSave() {
-    var saveId = messageLine.getAttribute("data-saveId");
-    var url = "https://www.googleapis.com/upload/drive/v3/files/" + saveId + "?uploadType=media";
-    
-    fetch(url, {
-      method: "PATCH",
-      headers: new Headers({
-        Authorization: "Bearer " + gapi.auth.getToken().access_token,
-        "Content-type": "plain/text; charset=UTF-8"
-      }), 
-      body: window.localStorage.getItem("banzuke")
-    }).then(value => {
-      //console.log("Saved progress to Drive successfully");
-      //showSave();
-    }).catch(err => console.error(err))
-  }
-
-  function b64_to_utf8(str) {
-    return decodeURIComponent(escape(window.atob(str)));
-  }
-
-  saveToDriveButton.addEventListener("click", function() {
-    if (window.localStorage.getItem("banzuke") !== null) {
-      document.getElementById("progressText").innerHTML = "Please wait...";
-
-      if (messageLine.innerHTML == "No save") 
-        uploadSave();
-      else 
-        updateSave();
-
-      messageLine.innerHTML = "From " + moment(new Date()).format("dddd, MMMM Do YYYY, h:mm:ss a");
-      document.getElementById("progressText").innerHTML = "Saved!";
-      setTimeout(function() {
-        document.getElementById("progressText").innerHTML = "";
-      }, 1000);
-    }
-  });
-
-  loadSaveButton.addEventListener("click", function() {
-    var saveId = messageLine.getAttribute("data-saveId");
-
-    document.getElementById("progressText").innerHTML = "Please wait...";
-
-    gapi.client.drive.files.get({
-      fileId: saveId, 
-      alt: "media"
-    }).then(function (res) {
-      var banzukeHtml = b64_to_utf8(btoa(res.body));
-
-      document.getElementById("tableLiner").innerHTML = banzukeHtml;
-      window.localStorage.setItem("banzuke", banzukeHtml);
-      redips.init();
-      document.getElementById("progressText").innerHTML = "";
-    });
-  });
-
-
-  var basho = "202301"; // The date of the basho just ended
 
   if (window.localStorage.getItem("banzuke1") !== null) {
     window.localStorage.removeItem("banzuke1");
@@ -507,6 +99,9 @@ window.onload = function() {
       }
     }
   }
+  
+  // Initialize REDIPS drag and drop after everything is loaded
+  redips.init();
 }
 
 function saveRadio(radioButton) {
@@ -675,16 +270,16 @@ redips.init = function () {
             else if (thisChg == 0) 
               thisChg = "─";
             break;
-          case 'J': thisChg = " ↑ "; break;
+          case 'J': thisChg = " ↑ "; break;
           default:
-            thisChg = " ↓ ";
+            thisChg = " ↓ ";
             thisRank = thisRank.substring(0, 1);
         }
       }
       else if (targetCellRank.charAt(0) == 'J') {
         targetCellRank = 'J';
         switch (thisRank.charAt(0)) {
-          case 'M': thisChg = " ↓ "; break;
+          case 'M': thisChg = " ↓ "; break;
           case 'J': thisChg = "⇄"; break;
           default:  thisChg = "!!!";
         }
@@ -692,7 +287,7 @@ redips.init = function () {
       else {
         switch (thisRank.charAt(0)) {
           case 'M': 
-            thisChg = " ↑ ";
+            thisChg = " ↑ ";
             targetCellRank = targetCellRank.substring(0, 1);
             break;
           case 'J': thisChg = "!!!"; break;
@@ -722,7 +317,7 @@ redips.init = function () {
 };
 
 redips.resetBanzuke = function() {
-  if (confirm("Reset the banzuke? This will not reset your save in Google Drive") == true) {
+  if (confirm("Reset the banzuke?") == true) {
     var b1Cell  = document.getElementsByTagName("td"), 
         b2Cell  = document.querySelectorAll(".b2"), 
         chgCell = document.getElementsByClassName("ch");
@@ -752,7 +347,8 @@ redips.resetBanzuke = function() {
   }
 };
 
-if (window.addEventListener)
-  window.addEventListener("load", redips.init, false);
-else if (window.attachEvent)
-  window.attachEvent("onload", redips.init);
+// Remove these as we'll call redips.init from within window.onload
+// if (window.addEventListener)
+//   window.addEventListener("load", redips.init, false);
+// else if (window.attachEvent)
+//   window.attachEvent("onload", redips.init);
